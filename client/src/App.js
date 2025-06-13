11fc5eeb999ba416ebeeab4ea78657e15e26b427
Deadbeat <147227234+Yukesh2@users.noreply.github.com>
2025-06-13 15:28:29 +0545

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Userid from './client/components/User';
 import {io} from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:4000');
+const socket = io(process.env.REACT_APP_SOCKET_URL);
 
 function Homepage(){
   return(
